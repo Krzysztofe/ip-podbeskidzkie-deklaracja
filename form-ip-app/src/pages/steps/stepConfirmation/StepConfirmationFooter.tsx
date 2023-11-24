c75179573ad0027ed330eff6f00e09d7da4ae442
krzysztofee <gitaragra@gmail.com>
2023-11-24 08:47:48 +0100

    Add in footer array with links data

--- form-ip-app/src/pages/steps/stepConfirmation/StepConfirmationFooter.tsx
+++ form-ip-app/src/pages/steps/stepConfirmation/StepConfirmationFooter.tsx
@@ -1,61 +1,44 @@
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { useTheme } from "@mui/system";
+import { Link } from "react-router-dom";
 import ImgLogoIp from "../../../images/ImgLogoIp";
 import { rwd } from "../../../utils/rwd";
-import { Link } from "react-router-dom";
-import React from "react";
-import { useTheme, Theme } from "@mui/system";
 
 enum TypographyVariant {
   FS16RG = "fs_16_rg",
   FS16SB = "fs_16_sb",
 }
 
-const createTypographyProps = (
-  variant: TypographyVariant,
-  fontWeight: number
-) => ({
+const createTypographyProps = (variant: TypographyVariant) => ({
   color: "info.dark",
   component: "p",
   variant,
-  style: { fontWeight },
 });
 
-const linkStyle = (theme: Theme) => ({
-  textDecoration: "none",
-  color: theme.palette.primary.main,
-});
+const texts = [
+  { variant: TypographyVariant.FS16SB, text: "KONTAKT:" },
+  {
+    variant: TypographyVariant.FS16RG,
+    text: "tel. 736-850-536",
+  },
+  {
+    variant: TypographyVariant.FS16RG,
+    text: "ipamazon@wp.pl",
+  },
+  { variant: TypographyVariant.FS16SB, text: "WWW:" },
 
-const footerTexts = (theme: Theme) => [
-  <Typography {...createTypographyProps(TypographyVariant.FS16SB, 600)}>
-    KONTAKT:
-  </Typography>,
-  <Typography {...createTypographyProps(TypographyVariant.FS16RG, 400)}>
-    tel. 736-850-536
-  </Typography>,
-  <Typography {...createTypographyProps(TypographyVariant.FS16RG, 400)}>
-    ipamazon@wp.pl
-  </Typography>,
-  <Typography {...createTypographyProps(TypographyVariant.FS16SB, 600)}>
-    WWW:
-  </Typography>,
-  <Typography {...createTypographyProps(TypographyVariant.FS16RG, 400)}>
-    <Link to="https://ozzip.pl/" style={linkStyle(theme)} target="_blank">
-      www.ozzip.pl
-    </Link>
-  </Typography>,
-  <Typography {...createTypographyProps(TypographyVariant.FS16SB, 600)}>
-    FACEBOOK:
-  </Typography>,
-  <Typography {...createTypographyProps(TypographyVariant.FS16RG, 400)}>
-    <Link
-      to="https://facebook.com/IPAmazon"
-      style={linkStyle(theme)}
-      target="_blank"
-    >
-      OZZ Inicjatywa Pracownicza Amazon
-    </Link>
-  </Typography>,
+  {
+    variant: TypographyVariant.FS16RG,
+    text: "www.ozzip.pl",
+    link: "https://ozzip.pl/",
+  },
+  { variant: TypographyVariant.FS16SB, text: "FACEBOOK:" },
+  {
+    variant: TypographyVariant.FS16RG,
+    text: "OZZ Inicjatywa Pracownicza Amazon",
+    link: "https://facebook.com/IPAmazon",
+  },
 ];
 
 const StepConfirmationFooter = () => {
@@ -75,9 +58,24 @@ const StepConfirmationFooter = () => {
           mr: rwd("auto", 4),
         }}
       >
-        {footerTexts(theme).map((paragraph, idx) => {
-          return <React.Fragment key={idx}>{paragraph}</React.Fragment>;
-        })}
+        {texts.map(({ variant, text, link }) => (
+          <Typography key={text} {...createTypographyProps(variant)}>
+            {link ? (
+              <Link
+                to={link}
+                style={{
+                  textDecoration: "none",
+                  color: theme.palette.primary.main,
+                }}
+                target="_blank"
+              >
+                {text}
+              </Link>
+            ) : (
+              text
+            )}
+          </Typography>
+        ))}
       </Box>
       <ImgLogoIp />
     </Box>
