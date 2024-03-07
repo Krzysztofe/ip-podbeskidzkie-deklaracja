340fc407275fb43155aab88c28ab1eeaf630937c
krzysztofee <gitaragra@gmail.com>
2024-03-07 21:23:10 +0100

    Add not found route in App

--- form-ip-app/src/App.tsx
+++ form-ip-app/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
       <Suspense fallback={<LoadingPage />}>
         <Routes>
           <Route path="/" element={<IndexSteps />} />
+          <Route path="/*" element={<IndexSteps />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
